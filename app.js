2c5a6e8518d2d16703f66ab274d65e97632e6290
Th0se <thomasap1464@gmail.com>
2022-11-07 03:31:53 +0700

    make initial files

---
+++ app.js
@@ -0,0 +1,115 @@
+/* horizontalDiv variables will be written as horizontalDiv(x)_(y), as in
+horizontalDiv1_2 will be the first horizontalDiv variable within 
+horizontalContainer2. */
+
+
+const horizontalDiv1_1 = document.createElement(`div`);
+horizontalDiv1_1.setAttribute(`id`, `horizontalDiv1_1`);
+horizontalDiv1_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv2_1 = document.createElement(`div`);
+horizontalDiv2_1.setAttribute(`id`, `horizontalDiv2_1`);
+horizontalDiv2_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv3_1 = document.createElement(`div`);
+horizontalDiv3_1.setAttribute(`id`, `horizontalDiv3_1`);
+horizontalDiv3_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv4_1 = document.createElement(`div`);
+horizontalDiv4_1.setAttribute(`id`, `horizontalDiv4_1`);
+horizontalDiv4_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv5_1 = document.createElement(`div`);
+horizontalDiv5_1.setAttribute(`id`, `horizontalDiv5_1`);
+horizontalDiv5_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv6_1 = document.createElement(`div`);
+horizontalDiv6_1.setAttribute(`id`, `horizontalDiv6_1`);
+horizontalDiv6_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv7_1 = document.createElement(`div`);
+horizontalDiv7_1.setAttribute(`id`, `horizontalDiv7_1`);
+horizontalDiv7_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv8_1 = document.createElement(`div`);
+horizontalDiv8_1.setAttribute(`id`, `horizontalDiv8_1`);
+horizontalDiv8_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv9_1 = document.createElement(`div`);
+horizontalDiv9_1.setAttribute(`id`, `horizontalDiv9_1`);
+horizontalDiv9_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv10_1 = document.createElement(`div`);
+horizontalDiv10_1.setAttribute(`id`, `horizontalDiv10_1`);
+horizontalDiv10_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv11_1 = document.createElement(`div`);
+horizontalDiv11_1.setAttribute(`id`, `horizontalDiv11_1`);
+horizontalDiv11_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv12_1 = document.createElement(`div`);
+horizontalDiv12_1.setAttribute(`id`, `horizontalDiv12_1`);
+horizontalDiv12_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv13_1 = document.createElement(`div`);
+horizontalDiv13_1.setAttribute(`id`, `horizontalDiv13_1`);
+horizontalDiv13_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv14_1 = document.createElement(`div`);
+horizontalDiv14_1.setAttribute(`id`, `horizontalDiv14_1`);
+horizontalDiv14_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv15_1 = document.createElement(`div`);
+horizontalDiv15_1.setAttribute(`id`, `horizontalDiv15_1`);
+horizontalDiv15_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv16_1 = document.createElement(`div`);
+horizontalDiv16_1.setAttribute(`id`, `horizontalDiv16_1`);
+horizontalDiv16_1.classList.add(`horizontalDiv`);
+
+const horizontalDiv1_2 = document.createElement(`div`);
+horizontalDiv1_2.setAttribute(`id`, `horizontalDiv1_2`);
+horizontalDiv1_2.classList.add(`horizontalDiv`);
+
+const horizontalDiv2_2 = document.createElement(`div`);
+horizontalDiv2_2.setAttribute(`id`, `horizontalDiv2_2`);
+horizontalDiv2_2.classList.add(`horizontalDiv`);
+
+const horizontalDiv3_2 = document.createElement(`div`);
+horizontalDiv3_2.setAttribute(`id`, `horizontalDiv3_2`);
+horizontalDiv3_2.classList.add(`horizontalDiv`);
+
+const horizontalDiv4_2 = document.createElement(`div`);
+horizontalDiv4_2.setAttribute(`id`, `horizontalDiv4_2`);
+horizontalDiv4_2.classList.add(`horizontalDiv`);
+
+const horizontalDiv5_2 = document.createElement(`div`);
+horizontalDiv5_2.setAttribute(`id`, `horizontalDiv5_2`);
+horizontalDiv5_2.classList.add(`horizontal`);
+
+
+
+
+const horizontalContainer1 = document.querySelector(`#horizontalContainer1`);
+
+const horizontalContainer2 = document.querySelector(`#horizontalContainer2`);
+
+
+horizontalContainer1.appendChild(horizontalDiv1_1);
+horizontalContainer1.appendChild(horizontalDiv2_1);
+horizontalContainer1.appendChild(horizontalDiv3_1);
+horizontalContainer1.appendChild(horizontalDiv4_1);
+horizontalContainer1.appendChild(horizontalDiv5_1);
+horizontalContainer1.appendChild(horizontalDiv6_1);
+horizontalContainer1.appendChild(horizontalDiv7_1);
+horizontalContainer1.appendChild(horizontalDiv8_1);
+horizontalContainer1.appendChild(horizontalDiv9_1);
+horizontalContainer1.appendChild(horizontalDiv10_1);
+horizontalContainer1.appendChild(horizontalDiv11_1);
+horizontalContainer1.appendChild(horizontalDiv12_1);
+horizontalContainer1.appendChild(horizontalDiv13_1);
+horizontalContainer1.appendChild(horizontalDiv14_1);
+horizontalContainer1.appendChild(horizontalDiv15_1);
+horizontalContainer1.appendChild(horizontalDiv16_1);
+
+horizontalContainer2.appendChild(horizontalDiv1_2);
