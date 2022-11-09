d6aeac8cc135ffd934eaef3d60cd0806828350ba
Th0se <thomasap1464@gmail.com>
2022-11-10 04:02:10 +0700

    make the 'mainContainer' fully functional

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-const makeHorizontalDiv = ((elementInput) => {
-    let result = [];
-    const numberOfDiv = 16;
-    for (let i = 0; i < numberOfDiv; i++) {
-        result.push(`<div class="grid"></div>`);
-    };
-    result = result.join('');
-    elementInput.innerHTML = result;
-});
+const mainContainer = document.querySelector(`#mainContainer`);
 
-const makeHorizontalContainer = ((elementInput) => {
-    let result = [];
-    const numberOfDiv = 16;
-    for (let i = 0; i < numberOfDiv; i++) {
-        result.push(`<div class="horizontalContainer"></div>`);
+const makeGrids = () => {
+    for (let i = 0; i < 256; i++) {
+        const cell = document.createElement(`div`);
+        cell.classList.add(`cell`);
+        cell.addEventListener(`mouseover`, (i) => {
+            i.target.style.backgroundColor = `green`;
+        });
+        mainContainer.appendChild(cell);
     };
-    result = result.join('');
-    elementInput.innerHTML = result;
-});
+};
 
-const enumerate = ((elementInput) => {
-    for (let i = 0; i < elementInput.length; i++) {
-        elementInput[i].setAttribute(`id`, `horizontalContainer${i + 1}`);
+const removeAllChildNodes = ((elementInput) => {
+    while (elementInput.firstChild) {
+        elementInput.removeChild(elementInput.firstChild);
     };
 });
-
-
 
 const resetButton = document.createElement(`div`);
 resetButton.setAttribute(`id`, `resetButton`);
@@ -33,46 +24,32 @@ resetButton.style.margin = `10px`;
 resetButton.style.padding = `10px`;
 resetButton.style.border = `1px solid black`;
 
-
-
 const sizeSlider = document.querySelector(`#sizeSlider`);
 const sizeDisplay = document.createElement(`p`);
 sizeDisplay.textContent = sizeSlider.value;
-
-
-
-const mainContainer = document.querySelector(`#mainContainer`);
-makeHorizontalContainer(mainContainer);
-mainContainer.style.display = `flex`;
-mainContainer.style.flexDirection = `column`;
-mainContainer.style.width = `100%`;
-mainContainer.style.height = `1000px`;
-mainContainer.style.margin = `auto`;
-
-
-const horizontalContainers = document.querySelectorAll('.horizontalContainer');
-enumerate(horizontalContainers);
-horizontalContainers.forEach((i) => {
-    makeHorizontalDiv(i);
-    i.style.height = `6%`;
-    i.style.width = `100%`;
-    i.style.display = `flex`;
-    i.style.flexDirection = `row`;
+sizeSlider.addEventListener(`input`, () => {
+    sizeDisplay.textContent = sizeSlider.value;
+    removeAllChildNodes(mainContainer);
+    mainContainer.setAttribute('style', `grid-template-columns: repeat(${sizeSlider.value}, 2fr); grid-template-rows: repeat(${sizeSlider.value}, 2fr);`);
+    for (let i = 0; i < sizeSlider.value*sizeSlider.value; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'green';
+        })
+        mainContainer.appendChild(div); 
+    }
 });
 
-const grid = document.querySelectorAll(`.grid`);
-grid.forEach((i) => {
-    i.style.height = `100%`;
-    i.style.width = `6%`;
-    i.style.border = `solid 1px black`;
-    i.style.backgroundColor = `green`;
-    i.addEventListener(`mouseover`, () => {
-        i.style.backgroundColor = `blue`;
-    })
-})
+const sizeControl = document.querySelector(`#sizeControl`);
+sizeControl.appendChild(sizeDisplay);
+sizeControl.style.display = `flex`;
+sizeControl.style.flexDirection = `column`;
+sizeControl.style.alignItems = "center";
 
 const topArea = document.querySelector('#topArea');
 topArea.appendChild(resetButton);
-topArea.appendChild(sizeDisplay);
 topArea.style.display = `flex`;
 topArea.style.justifyContent = 'center';
+
+makeGrids();
